348d70599c6c2b52f96195a0d0cfef3bcdef024a
sebastian marcet <smarcet@gmail.com>
2023-06-12 18:11:56 -0700

    Revert "Add new fields and collections for sponsor and tiers (#4)"
    This reverts commit 458e0425f1330d851115ac9812b9395acd77a004.

--- src/cms/config/collections/configurationsCollection/index.js
+++ src/cms/config/collections/configurationsCollection/index.js
@@ -7,7 +7,6 @@ import ads from "./ads";
 import navbar from "./navbar";
 import footer from "./footer";
 import postersPages from "./postersPages";
-import sponsorTiers from "./sponsorTiers";
 
 const configurationsCollection = {
   ...collectionDefaults({
@@ -19,8 +18,7 @@ const configurationsCollection = {
     ads,
     navbar,
     footer,
-    postersPages,
-    sponsorTiers
+    postersPages
   ]
 };
 
