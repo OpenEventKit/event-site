3835f7edfe713b9300ac9abc997224e2f44a26d6
Tomás Castillo <tomrndom@users.noreply.github.com>
2023-12-07 11:47:06 -0300

    Add new payments category on CMS, add apple domains (#54)
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

---
+++ src/cms/config/collections/configurationsCollection/payments/index.js
@@ -0,0 +1,26 @@
+import {
+  fileField,  
+} from "../../../fields";
+
+import {  
+  PAYMENTS_FILE_PATH,
+  APPLE_PAY_DOMAIN_FILE_PATH,
+  APPLE_PAY_DOMAIN_FILE_NAME
+} from "@utils/filePath";
+
+const payments = {  
+  label: "Payments",
+  name: "payments",
+  file: PAYMENTS_FILE_PATH,
+  fields: [
+    fileField({
+      label: "Apply Pay Domain File",
+      name: "applePayDomainFile",
+      file: APPLE_PAY_DOMAIN_FILE_NAME,
+      media_folder: APPLE_PAY_DOMAIN_FILE_PATH,
+      public_folder: APPLE_PAY_DOMAIN_FILE_PATH,
+    }),            
+  ],        
+};
+
+export default payments;
