d80134e4f1a35e7b09cd274a88758a5a3faac925
Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>
2024-03-05 11:33:06 -0300

    Adds google tag manager gatsby plugin.

--- gatsby-config.js
+++ gatsby-config.js
@@ -177,6 +177,13 @@ const plugins = [
       }
     }
   },
+  {
+    resolve: "gatsby-plugin-google-tagmanager",
+    options: {
+      id: process.env.GATSBY_GOOGLE_TAGMANAGER_ID,
+      includeInDevelopment: false,
+    },
+  },
   "gatsby-plugin-netlify", // make sure to keep it last in the array
 ];
 
