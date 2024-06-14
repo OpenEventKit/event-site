OpenEventKit/event-site

commit 32ee87868d78027444cf0fdb8a1a4438ed0c2e2f

Fixes event click handler set on marketing page's schedule. Fixes prop type mismatch.

src/templates/marketing-page-template/MainColumn.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { navigate } from "gatsby";
 import PropTypes from "prop-types";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Mdx from "@mdx-js/runtime";
@@ -15,55 +16,63 @@ const shortcodes = {
   img: ResponsiveImage
 };
 
-const MainColumn = ({ widgets, scheduleProps, lastDataSync, fullWidth, maxHeight }) => (
-  <div
-    className={`column pt-6 pb-5 px-6 ${!fullWidth ? "is-half" : ""} ${styles.mainColumn || ""}`}
-    style={{ maxHeight: !fullWidth && maxHeight ? maxHeight : "none", overflowY: "auto" }}
-  >
-    <Container>
-      {widgets?.content?.display && widgets?.content?.body && (
-      <Mdx components={shortcodes}>
-        {widgets.content.body}
-      </Mdx>
-      )}
-      {widgets?.schedule?.display && (
-      <>
-        <h2><b>{widgets.schedule.title}</b></h2>
-        <LiteScheduleComponent
-          {...scheduleProps}
-          lastDataSync={lastDataSync}
-          id={`marketing_lite_schedule_${lastDataSync}`}
-          page="marketing-site"
-          showAllEvents={true}
-          showSearch={false}
-          showNav={true}
-        />
-      </>
-      )}
-      {widgets?.disqus?.display && (
-      <>
-        <h2><b>{widgets.disqus.title}</b></h2>
-        <DisqusComponent page="marketing-site" />
-      </>
-      )}
-     {widgets?.image?.display && widgets?.image?.image.src && (
-      <>
-        <h2><b>{widgets.image.title}</b></h2>
-        <br />
-        <GatsbyImage image={getImage(widgets.image.image.src)} alt={widgets.image.image.alt ?? ""} />
-      </>
-      )}
-    </Container>
-  </div>
-);
+const onEventClick = (ev) => navigate(`/a/event/${ev.id}`);
+
+const MainColumn = ({ widgets, summitPhase, isLoggedUser, onEventClick, lastDataSync, fullWidth, maxHeight }) => {
+  const { content, schedule, disqus, image } = widgets || {};
+  
+  const scheduleProps = schedule && isLoggedUser && summitPhase !== PHASES.BEFORE ? { onEventClick } : {};
+
+  return (
+    <div
+      className={`column pt-6 pb-5 px-6 ${!fullWidth ? "is-half" : ""} ${styles.mainColumn || ""}`}
+      style={{ maxHeight: !fullWidth && maxHeight ? maxHeight : "none", overflowY: "auto" }}
+    >
+      <Container>
+        {content?.display && content?.body && (
+          <Mdx components={shortcodes}>
+            {content.body}
+          </Mdx>
+        )}
+        {schedule?.display && (
+          <>
+            <h2><b>{schedule.title}</b></h2>
+            <LiteScheduleComponent
+              {...scheduleProps}
+              lastDataSync={lastDataSync}
+              id={`marketing_lite_schedule_${lastDataSync}`}
+              page="marketing-site"
+              showAllEvents={true}
+              showSearch={false}
+              showNav={true}
+            />
+          </>
+        )}
+        {disqus?.display && (
+          <>
+            <h2><b>{disqus.title}</b></h2>
+            <DisqusComponent page="marketing-site" />
+          </>
+        )}
+        {image?.display && image?.image.src && (
+          <>
+            <h2><b>{image.title}</b></h2>
+            <br/>
+            <GatsbyImage image={getImage(image.image.src)} alt={image.image.alt ?? ""} />
+          </>
+        )}
+      </Container>
+    </div>
+  );
+};
 
 MainColumn.propTypes = {
   widgets: PropTypes.object,
-  scheduleProps: PropTypes.object,
+  summitPhase: PropTypes.number,
+  isLoggedUser: PropTypes.bool,
   lastDataSync: PropTypes.number,
   fullWidth: PropTypes.bool,
   maxHeight: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
 };
 
 export default MainColumn;
-

src/templates/marketing-page-template/index.js
@@ -1,11 +1,9 @@
-import React, { useRef, useState, useCallback } from "react";
+import React, { useRef, useState } from "react";
 import PropTypes from "prop-types";
-import { navigate } from "gatsby";
 import Layout from "../../components/Layout";
 import AttendanceTrackerComponent from "../../components/AttendanceTrackerComponent";
 import MarketingHero from "../../components/MarketingHero";
 import Countdown from "../../components/Countdown";
-import Link from "../../components/Link";
 import MainColumn from "./MainColumn";
 import Masonry from "./Masonry";
 import { useResize } from "@utils/hooks";
@@ -13,17 +11,18 @@ import { PHASES } from "@utils/phasesUtils";
 
 import styles from "./styles.module.scss";
 
-const onEventClick = (ev) => navigate(`/a/event/${ev.id}`);
-
 const MarketingPageTemplate = ({ data, location, summit, summitPhase, isLoggedUser, lastDataSync }) => {
   const masonryRef = useRef();
   const [rightColumnHeight, setRightColumnHeight] = useState();
+
   const onResize = () => {
     if (masonryRef?.current) {
       setRightColumnHeight(masonryRef.current.firstChild.clientHeight);
     }
   };
+  
   useResize(onResize);
+
   const {
     marketingPageJson: {
       hero,
@@ -32,13 +31,9 @@ const MarketingPageTemplate = ({ data, location, summit, summitPhase, isLoggedUs
       masonry
     } = {}
   } = data || {};
-  const scheduleProps = useCallback(() => {
-    if (widgets?.schedule && isLoggedUser && summitPhase !== PHASES.BEFORE) {
-      return { onEventClick };
-    }
-    return {};
-  }, [widgets, isLoggedUser, summitPhase]);
+
   const shouldRenderMasonry = masonry?.display;
+
   return (
     <Layout marketing={true} location={location}>
       <AttendanceTrackerComponent />
@@ -47,7 +42,8 @@ const MarketingPageTemplate = ({ data, location, summit, summitPhase, isLoggedUs
       <div className="columns is-marginless">
         <MainColumn
           widgets={widgets}
-          scheduleProps={scheduleProps}
+          summitPhase={summitPhase}
+          isLoggedUser={isLoggedUser}
           maxHeight={rightColumnHeight}
           fullWidth={!shouldRenderMasonry}
           lastDataSync={lastDataSync}

src/components/MarketingHero/ImagesColumn.js
@@ -22,7 +22,7 @@ const ImagesColumn = ({ images }) => {
           <img
             key={index}
             src={getSrc(image.src)}
-            alt={image.alt}
+            alt={image.alt ?? ""}
           />
           ))}
         </Slider>
@@ -30,7 +30,7 @@ const ImagesColumn = ({ images }) => {
         <GatsbyImage
           className={styles.singleImage}
           image={getImage(images[0].src)}
-          alt={images[0].alt}
+          alt={images[0].alt ?? ""}
         />
       )}
     </div>

commit 0d279c142cfd8f8bac9edf01f0aa0af7005b52ab

Fixes bug introduced in 32ee87868d78027444cf0fdb8a1a4438ed0c2e2f commit.

src/templates/marketing-page-template/MainColumn.js
@@ -9,6 +9,8 @@ import DisqusComponent from "../../components/DisqusComponent";
 import ResponsiveImage from "../../components/ResponsiveImage";
 import Link from "../../components/Link";
 
+import { PHASES } from "@utils/phasesUtils";
+
 import styles from "./styles.module.scss";
 
 const shortcodes = {

src/templates/marketing-page-template/index.js
@@ -7,7 +7,6 @@ import Countdown from "../../components/Countdown";
 import MainColumn from "./MainColumn";
 import Masonry from "./Masonry";
 import { useResize } from "@utils/hooks";
-import { PHASES } from "@utils/phasesUtils";
 
 import styles from "./styles.module.scss";
 