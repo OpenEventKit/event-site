8e1bd5d3dd048ae9bd3dd3d2e67420a94f94e832
Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>
2024-07-03 15:55:15 -0300

    Makes CookieManager a singleton.

--- src/utils/cookies/CookieManager.js
+++ src/utils/cookies/CookieManager.js
@@ -1,13 +1,22 @@
 import CookieManagerProvider from "./CookieManagerProvider";
 
 class CookieManager {
+  static instance;
+
   constructor(provider, services) {
+    if (CookieManager.instance) {
+      return CookieManager.instance;
+    }
+
     if (!(provider instanceof CookieManagerProvider)) {
       throw new Error("Provider must implement CookieManagerProvider interface");
     }
+
     this.provider = provider;
     this.services = services;
-    this.provider.init(this.services)
+    this.provider.init(this.services);
+
+    CookieManager.instance = this;
   }
 
   getConsents = () => this.provider.getConsents();
