eea0084fa6135bf206a2ce47639561b0e2eb236f
Tomás Castillo <tcastilloboireau@gmail.com>
2025-05-09 20:24:43 -0300

    remove console log
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- gatsby-node.js
+++ gatsby-node.js
@@ -251,9 +251,6 @@ const SSR_getSummit = async (baseUrl, summitId, accessToken) => {
   
   apiUrl.addQuery('t', Date.now());
 
-  console.log("CHECK!", apiUrl.toString());
-
-
   return await axios.get(
     apiUrl.toString()
   )
