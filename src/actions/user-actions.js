c21472d627ede9f913d0bacb26c9b56f564a2a17
Tomás Castillo <tcastilloboireau@gmail.com>
2025-08-25 15:18:20 -0300

    add missing text, fix loader
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/actions/user-actions.js
+++ src/actions/user-actions.js
@@ -686,8 +686,6 @@ export const getRSVPInvitation = (token, eventId) => async (dispatch) => {
   )(params)(dispatch)
     .catch(({ err }) => {
       let code = err.status;
-      console.log("CHJEC!", err)
-      dispatch(stopLoading());
       let msg = '';
       switch (code) {
         case 401:
