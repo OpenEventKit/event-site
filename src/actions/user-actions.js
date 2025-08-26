1c682d31ec8fd4b1d4b8266de44014a6ccecaeaf
Tomás Castillo <tcastilloboireau@gmail.com>
2025-08-26 14:38:13 -0300

    update getRsvpInvitation fields, adjust style
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/actions/user-actions.js
+++ src/actions/user-actions.js
@@ -677,9 +677,8 @@ export const rejectInvitation = (token) => async (dispatch) => {
 
 export const getRSVPInvitation = (token, eventId) => async (dispatch) => {
   let params = {
-    expand: "event,event.speakers",
-    fields: "id,is_accepted,status,event.title,event.description,event.moderator_speaker_id,event.speakers.first_name,event.speakers.last_name,event.speakers.company,event.speakers.title,event.speakers.pic",
-    relations: "event.none,event.speakers.none"
+    fields: "status,event_id",
+    relations: "none"
   };
 
   return getRequest(
@@ -722,7 +721,7 @@ export const getRSVPInvitation = (token, eventId) => async (dispatch) => {
 
 export const acceptRSVPInvitation = (token, eventId) => async (dispatch) => {
 
-  let params = {    
+  let params = {
     expand: "rsvp",
     fields: "status,event_id,rsvp.seat_type,rsvp.event_id",
     relations: "event.none,rsvp.none"
@@ -740,9 +739,8 @@ export const acceptRSVPInvitation = (token, eventId) => async (dispatch) => {
 }
 
 export const declineRSVPInvitation = (token, eventId) => async (dispatch) => {
-
-  let params = {    
-    fields: "status,event.id",
+  let params = {
+    fields: "status,event_id",
     relations: "event.none"
   };
 
