2700a63359260c90a4d30ebb1e49c773d88923f2
smarcet <smarcet@gmail.com>
2025-08-25 22:39:56 -0300

    fix: ADD_TO_SCHEDULE payload on RSVP_INVITATION_ACCEPTED

--- src/actions/user-actions.js
+++ src/actions/user-actions.js
@@ -780,7 +780,7 @@ export const acceptRSVPInvitation = (token, eventId) => async (dispatch) => {
     null,
     customRSVPHandler
   )(params)(dispatch)
-    .then((payload)=>   dispatch(createAction(ADD_TO_SCHEDULE)(payload.event)))
+    .then((payload)=>   dispatch(createAction(ADD_TO_SCHEDULE)(payload.response.event)))
     .catch((err) => console.log("ERROR: ", err))
     .finally(() => dispatch(stopLoading()));
 }
