b380aa3aaab4b7188508e3335e8fbbcdc7f6f7c6
smarcet <smarcet@gmail.com>
2025-09-10 21:57:59 -0300

    fix: avoid data corruption on data fetch

--- src/workers/feeds.worker.js
+++ src/workers/feeds.worker.js
@@ -6,101 +6,96 @@ import speakersBuildJson from "data/speakers.json";
 import speakersIDXBuildJson from "data/speakers.idx.json";
 
 import {
-  bucket_getSummit,
-  bucket_getEvents,
-  bucket_getEventsIDX,
-  bucket_getSpeakers,
-  bucket_getSpeakersIDX
+    bucket_getSummit,
+    bucket_getEvents,
+    bucket_getEventsIDX,
+    bucket_getSpeakers,
+    bucket_getSpeakersIDX
 } from "../actions/update-data-actions";
 
 import {
-  SUMMIT_FILE_PATH,
-  EVENTS_FILE_PATH,
-  EVENTS_IDX_FILE_PATH,
-  SPEAKERS_FILE_PATH,
-  SPEAKERS_IDX_FILE_PATH
+    SUMMIT_FILE_PATH,
+    EVENTS_FILE_PATH,
+    EVENTS_IDX_FILE_PATH,
+    SPEAKERS_FILE_PATH,
+    SPEAKERS_IDX_FILE_PATH
 } from "../utils/filePath";
+
+const isNonEmptyObject = (v) =>
+    v && typeof v === "object" && !Array.isArray(v) && Object.keys(v).length > 0;
+
+const isNonEmptyArray = (v) => Array.isArray(v) && v.length > 0;
+
+const pick = (result, expect) => {
+    if (!result || typeof result !== "object") {
+        return {accepted: false, data: null, lastModified: 0};
+    }
+    const {file, lastModified} = result;
+    const ok =
+        expect === "object" ? isNonEmptyObject(file) :
+            expect === "array" ? isNonEmptyArray(file) : false;
+
+    return ok
+        ? {accepted: true, data: file, lastModified: lastModified}
+        : {accepted: false, data: null, lastModified: 0};
+};
 
 /* eslint-disable-next-line no-restricted-globals */
 self.onmessage = async ({data: {summitId, staticJsonFilesBuildTime}}) => {
-  staticJsonFilesBuildTime = JSON.parse(staticJsonFilesBuildTime);
+    staticJsonFilesBuildTime = JSON.parse(staticJsonFilesBuildTime);
 
-  console.log(`feeds worker running for ${summitId} ....`)
-  const calls = [];
+    console.log(`feeds worker running for ${summitId} ....`)
+    const calls = [];
 
-  // events
-  let buildTime = staticJsonFilesBuildTime.find(e => e.file === EVENTS_FILE_PATH).build_time;
+    // events
+    let buildTime = staticJsonFilesBuildTime.find(e => e.file === EVENTS_FILE_PATH).build_time;
 
-  calls.push(bucket_getEvents(summitId, buildTime));
+    calls.push(bucket_getEvents(summitId, buildTime));
 
-  buildTime = staticJsonFilesBuildTime.find(e => e.file === EVENTS_IDX_FILE_PATH).build_time;
-  calls.push(bucket_getEventsIDX(summitId, buildTime));
+    buildTime = staticJsonFilesBuildTime.find(e => e.file === EVENTS_IDX_FILE_PATH).build_time;
+    calls.push(bucket_getEventsIDX(summitId, buildTime));
 
-  // summit
-  buildTime = staticJsonFilesBuildTime.find(e => e.file === SUMMIT_FILE_PATH).build_time;
-  calls.push(bucket_getSummit(summitId, buildTime));
+    // summit
+    buildTime = staticJsonFilesBuildTime.find(e => e.file === SUMMIT_FILE_PATH).build_time;
+    calls.push(bucket_getSummit(summitId, buildTime));
 
-  //speakers
-  buildTime = staticJsonFilesBuildTime.find(e => e.file === SPEAKERS_FILE_PATH).build_time;
-  calls.push(bucket_getSpeakers(summitId, buildTime));
+    //speakers
+    buildTime = staticJsonFilesBuildTime.find(e => e.file === SPEAKERS_FILE_PATH).build_time;
+    calls.push(bucket_getSpeakers(summitId, buildTime));
 
-  buildTime = staticJsonFilesBuildTime.find(e => e.file === SPEAKERS_IDX_FILE_PATH).build_time;
-  calls.push(bucket_getSpeakersIDX(summitId, buildTime));
+    buildTime = staticJsonFilesBuildTime.find(e => e.file === SPEAKERS_IDX_FILE_PATH).build_time;
+    calls.push(bucket_getSpeakersIDX(summitId, buildTime));
 
-  Promise.all(calls)
-    .then((values) => {
-      let lastModified = settings.lastBuild;
-      let eventsData = values[0];
-      let eventsIDXData = values[1];
-      let summitData = values[2];
-      let speakersData = values[3];
-      let speakersIXData = values[4];
+    Promise.all(calls)
+        .then((values) => {
+            let lastModified = settings.lastBuild;
+            let eventsData = values[0];
+            let eventsIDXData = values[1];
+            let summitData = values[2];
+            let speakersData = values[3];
+            let speakersIXData = values[4];
 
-      // if null , then set the SSR content
-      // summit
-      if (summitData && summitData?.file){
-        if(summitData.lastModified > lastModified)
-          lastModified = summitData.lastModified;
-        summitData = summitData.file;
-      }
-      else
-        summitData = summitBuildJson;
-      // events
-      if (eventsData && eventsData?.file){
-        if(eventsData.lastModified > lastModified)
-          lastModified = eventsData.lastModified;
-        eventsData = eventsData.file;
-      }
-      else
-        eventsData = eventsBuildJson;
-      // events idx
-      if (eventsIDXData && eventsIDXData?.file){
-        if(eventsIDXData.lastModified > lastModified)
-          lastModified = eventsIDXData.lastModified;
-        eventsIDXData = eventsIDXData.file;
-      }
-      else
-        eventsIDXData = eventsIDXBuildJson;
-      // speakers
-      if (speakersData && speakersData?.file){
-        if(speakersData.lastModified > lastModified)
-          lastModified = speakersData.lastModified;
-        speakersData = speakersData.file;
-      }
-      else
-        speakersData = speakersBuildJson;
-      // speakers idx
-      if (speakersIXData && speakersIXData?.file){
-        if(speakersIXData.lastModified > lastModified)
-          lastModified = speakersIXData.lastModified;
-        speakersIXData = speakersIXData.file;
-      }
-      else
-        speakersIXData = speakersIDXBuildJson;
+            // if null , then set the SSR content
+            // summit
+            const summitDataPicked = pick(summitData, "object");
+            summitData = summitDataPicked.accepted && summitDataPicked.lastModified > lastModified ? summitDataPicked.data : summitBuildJson;
+            // events
+            const eventsDataPicked = pick(eventsData, "array");
+            eventsData = eventsDataPicked.accepted && eventsDataPicked.lastModified > lastModified ? eventsDataPicked.data : eventsBuildJson;
+            // events idx
+            const eventsIDXDataPicked = pick(eventsIDXData, "object");
+            eventsIDXData = eventsIDXDataPicked.accepted && eventsIDXDataPicked.lastModified > lastModified ? eventsIDXDataPicked.data : eventsIDXBuildJson;
+            // speakers
+            const speakersDataPicked = pick(speakersData, "array");
+            speakersData = speakersDataPicked.accepted && speakersDataPicked.lastModified > lastModified ? speakersDataPicked.data : speakersBuildJson;
+            // speakers idx
+            const speakersIXDataPicked = pick(speakersIXData, "object");
+            speakersIXData = speakersIXDataPicked.accepted && speakersIXDataPicked.lastModified > lastModified ? speakersIXDataPicked.data : speakersIDXBuildJson;
 
-      /* eslint-disable-next-line no-restricted-globals */
-      self.postMessage({
-        eventsData, summitData, speakersData, eventsIDXData, speakersIXData, lastModified
-      });
-    });
+
+            /* eslint-disable-next-line no-restricted-globals */
+            self.postMessage({
+                eventsData, summitData, speakersData, eventsIDXData, speakersIXData, lastModified
+            });
+        });
 };
