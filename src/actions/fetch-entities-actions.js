08130a482d78f2400991e50b1b6341109cb01537
Tomás Castillo <tcastilloboireau@gmail.com>
2025-10-09 19:53:09 -0300

    feat: implement etag on fetch entities actions
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/actions/fetch-entities-actions.js
+++ src/actions/fetch-entities-actions.js
@@ -3,6 +3,50 @@ import SummitAPIRequest from "../utils/build-json/SummitAPIRequest";
 import EventAPIRequest from "../utils/build-json/EventsAPIRequest";
 import SpeakersAPIRequest from "../utils/build-json/SpeakersAPIRequest";
 
+const etagCache = {};
+
+const byLowerCase = toFind => value => toLowerCase(value) === toFind;
+const toLowerCase = value => value.toLowerCase();
+const getKeys = headers => Object.keys(headers);
+
+const getHeaderCaseInsensitive = (headerName, headers = {}) => {
+    const key = getKeys(headers).find(byLowerCase(headerName));
+    return key ? headers[key] : undefined;
+};
+
+const fetchWithEtag = async (url, cacheKey) => {
+    const headers = {};
+
+    if (etagCache.hasOwnProperty(cacheKey)) {
+        const { etag } = etagCache[cacheKey];
+        if (etag) {
+            headers['If-None-Match'] = etag;
+        }
+    }
+
+    const res = await fetch(url, {
+        method: 'GET',
+        cache: "no-store",
+        headers,
+    });
+
+    if (res.status === 304 && etagCache.hasOwnProperty(cacheKey)) {
+        const { body } = etagCache[cacheKey];
+        return body;
+    }
+
+    if (res.status === 200) {
+        const data = await res.json();
+        const responseETAG = getHeaderCaseInsensitive('etag', res.headers);
+        if (responseETAG) {
+            etagCache[cacheKey] = { etag: responseETAG, body: data };
+        }
+        return data;
+    }
+
+    return null;
+};
+
 /**
  * @param summitId
  * @param eventId
@@ -18,16 +62,9 @@ export const fetchEventById = async (summitId, eventId, accessToken = null) => {
     }
 
     const apiUrlWithParams = EventAPIRequest.build(apiUrl);
+    const cacheKey = apiUrlWithParams;
 
-    return fetch(apiUrlWithParams, {
-        method: 'GET',
-        cache: "no-store",
-    }).then(async (response) => {
-        if (response.status === 200) {
-            return await response.json();
-        }
-        return null;
-    });
+    return fetchWithEtag(apiUrlWithParams, cacheKey);
 }
 
 /**
@@ -39,15 +76,10 @@ export const fetchEventById = async (summitId, eventId, accessToken = null) => {
 export const fetchStreamingInfoByEventId = async (summitId, eventId, accessToken) => {
     const apiUrl = URI(`${process.env.GATSBY_SUMMIT_API_BASE_URL}/api/v1/summits/${summitId}/events/${eventId}/published/streaming-info`);
     apiUrl.addQuery('access_token', accessToken);
-    return fetch(apiUrl.toString(), {
-        method: 'GET',
-        cache: "no-store",
-    }).then(async (response) => {
-        if (response.status === 200) {
-            return await response.json();
-        }
-        return null;
-    });
+    const url = apiUrl.toString();
+    const cacheKey = url;
+
+    return fetchWithEtag(url, cacheKey);
 }
 
 /**
@@ -64,19 +96,13 @@ export const fetchEventTypeById = async (summitId, eventTypeId, accessToken = nu
         apiUrl.addQuery('access_token', accessToken);
     }
 
-    return fetch(apiUrl.toString(), {
-        method: 'GET',
-        cache: "no-store",
-    }).then(async (response) => {
-        if (response.status === 200) {
-            return await response.json();
-        }
-        return null;
-    });
+    const url = apiUrl.toString();
+    const cacheKey = url;
+
+    return fetchWithEtag(url, cacheKey);
 }
 
 /**
- *
  * @param summitId
  * @param locationId
  * @param expand
@@ -94,19 +120,13 @@ export const fetchLocationById = async (summitId, locationId, expand, accessToke
     if (expand)
         apiUrl.addQuery('expand', expand);
 
-    return fetch(apiUrl.toString(), {
-        method: 'GET',
-        cache: "no-store",
-    }).then(async (response) => {
-        if (response.status === 200) {
-            return await response.json();
-        }
-        return null;
-    });
+    const url = apiUrl.toString();
+    const cacheKey = url;
+
+    return fetchWithEtag(url, cacheKey);
 }
 
 /**
- *
  * @param summitId
  * @param speakerId
  * @param accessToken
@@ -122,20 +142,12 @@ export const fetchSpeakerById = async (summitId, speakerId, accessToken = null) 
     }
 
     const apiUrlWithParams = SpeakersAPIRequest.build(apiUrl);
+    const cacheKey = apiUrlWithParams;
 
-    return fetch(apiUrlWithParams, {
-        method: 'GET',
-        cache: "no-store",
-    }).then(async (response) => {
-        if (response.status === 200) {
-            return await response.json();
-        }
-        return null;
-    });
+    return fetchWithEtag(apiUrlWithParams, cacheKey);
 }
 
 /**
- *
  * @param summitId
  * @param accessToken
  * @returns {Promise<Response>}
@@ -149,16 +161,9 @@ export const fetchSummitById = async (summitId, accessToken = null) => {
     }
 
     const apiUrlWithParams = SummitAPIRequest.build(apiUrl);
+    const cacheKey = apiUrlWithParams;
 
-    return fetch(apiUrlWithParams, {
-        method: 'GET',
-        cache: "no-store",
-    }).then(async (response) => {
-        if (response.status === 200) {
-            return await response.json();
-        }
-        return null;
-    });
+    return fetchWithEtag(apiUrlWithParams, cacheKey);
 }
 
 /**
@@ -171,7 +176,7 @@ export const fetchTrackById = async (summitId, trackId, accessToken = null) => {
     let apiUrl = URI(`${process.env.GATSBY_SUMMIT_API_BASE_URL}/api/public/v1/summits/${summitId}/tracks/${trackId}`);
 
     const fields = [
-        "id", "name", "code", "order", "parent_id", "color","text_color",
+        "id", "name", "code", "order", "parent_id", "color", "text_color",
         "subtracks.id", "subtracks.name", "subtracks.code", "subtracks.order",
         "subtracks.parent_id", "subtracks.color", "subtracks.text_color",
     ];
@@ -182,13 +187,8 @@ export const fetchTrackById = async (summitId, trackId, accessToken = null) => {
     apiUrl.addQuery('relations', relations.join(','));
     apiUrl.addQuery('expand', expand.join(','));
 
-    return fetch(apiUrl.toString(), {
-        method: 'GET',
-        cache: "no-store",
-    }).then(async (response) => {
-        if (response.status === 200) {
-            return await response.json();
-        }
-        return null;
-    });
+    const url = apiUrl.toString();
+    const cacheKey = url;
+
+    return fetchWithEtag(url, cacheKey);
 }
