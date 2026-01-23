ac075bd883c3900b4647666ee45c130ae7d50db0
Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>
2026-01-23 13:56:51 -0300

    refactor: move TICKET_ATTENDEE_KEYS to src/utils/ticketConstants.js
    Addresses PR review comment to separate constants from action creators,
    following the existing pattern of domain-scoped constant files.

---
+++ src/utils/ticketConstants.js
@@ -0,0 +1,10 @@
+const TICKET_ATTENDEE_KEYS = {
+  email: 'attendee_email',
+  firstName: 'attendee_first_name',
+  lastName: 'attendee_last_name',
+  company: 'attendee_company',
+  disclaimerAccepted: 'disclaimer_accepted',
+  extraQuestions: 'extra_questions'
+};
+
+export { TICKET_ATTENDEE_KEYS };
